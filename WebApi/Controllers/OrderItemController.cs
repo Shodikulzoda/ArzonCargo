70e552c459dc07ad720b4cf1500ce11689dfc145
Bobo <mukarrammukarramov09@gmail.com>
2025-06-17 00:28:22 +0500

    new things

--- WebApi/Controllers/OrderItemController.cs
+++ WebApi/Controllers/OrderItemController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers;
+
 [ApiController]
 [Route("[controller]/[action]")]
 public class OrderItemController : ControllerBase
@@ -11,23 +12,25 @@ public class OrderItemController : ControllerBase
     {
         return Ok(orderItem);
     }
-    
+
     [HttpGet]
     public IActionResult GetOrderItem(Guid id)
     {
         return Ok(id);
     }
+
     [HttpGet]
     public IActionResult GetAll()
     {
         return Ok();
     }
+
     [HttpPut]
     public IActionResult UpdateOrderItem(OrderItem orderItem)
     {
         return Ok(orderItem);
     }
-    
+
     [HttpDelete]
     public IActionResult DeleteOrderItem(Guid id)
     {
