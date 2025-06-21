17a92f2d89e02843249f9d7d366a3d7784ec83eb
mukarramov <165103515+mukarramov@users.noreply.github.com>
2025-06-21 15:10:52 +0500

    Delete Application directory

--- Application/Interfaces/IBaseRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace Application.Interfaces;
-
-public interface IBaseRepository<T> where T : class
-{
-    public IQueryable<T> Querable { get; set; }
-
-    public Task<bool> Delete(int id);
-}
