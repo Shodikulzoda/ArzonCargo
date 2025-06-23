36f21000d1c6131f16a18e8d149c33ad79cd4ac3
Bobo <mukarrammukarramov09@gmail.com>
2025-06-23 18:49:11 +0500

    added test pagination for user

---
+++ Application/Dtos/Request/UserRequest.cs
@@ -0,0 +1,8 @@
+namespace Application.Dtos.Request;
+
+public class UserRequest
+{
+    public string? Name { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+}
