8d4b48044ceb9366ca7d70ae2b7b05f410a24fec
Alisher Shodikulzoda Olimov <151871971+Shodikulzoda@users.noreply.github.com>
2025-06-25 16:25:28 +0500

    Delete Application/Dtos/Request directory

--- Application/Dtos/Request/UserRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace Application.Dtos.Request;
-
-public class UserRequest
-{
-    public string? Name { get; set; }
-    public string? Phone { get; set; }
-    public string? Address { get; set; }
-}
