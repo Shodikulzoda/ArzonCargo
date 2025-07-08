b83e2dba3e931292b299527a0b7326d91963dc2c
Bobo <mukarrammukarramov09@gmail.com>
2025-07-08 15:37:09 +0500

    added accesses to pages (admin and adder)

--- Stocky.Client/Components/Service/AuthService.cs
+++ Stocky.Client/Components/Service/AuthService.cs
@@ -1,35 +1,42 @@
-﻿using Microsoft.JSInterop;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.JSInterop;
 
 namespace MudBlazor.Components.Service
 {
-    public class AuthService
+    public class AuthService(IJSRuntime jsRuntime)
     {
         private const string TokenKey = "authToken";
-        private readonly IJSRuntime _jsRuntime;
-
         public string Token { get; private set; } = "";
-
-        public AuthService(IJSRuntime jsRuntime)
-        {
-            _jsRuntime = jsRuntime;
-        }
+        public string? Role { get; private set; }
 
         public void SetToken(string token)
         {
             Token = token;
-            _ = _jsRuntime.InvokeVoidAsync("localStorage.setItem", TokenKey, token);
+            _ = jsRuntime.InvokeVoidAsync("localStorage.setItem", TokenKey, token);
+
+            var handler = new JwtSecurityTokenHandler();
+
+            var jwtToken = handler.ReadJwtToken(token);
+
+            // Standard claim type for role is "role" or ClaimTypes.Role
+            Role = jwtToken.Claims.FirstOrDefault(c =>
+                c.Type is ClaimTypes.Role or "role")?.Value;
         }
 
         public async Task LoadTokenAsync()
         {
-            var token = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", TokenKey);
-            Token = token ?? "";
+            var token = await jsRuntime.InvokeAsync<string>("localStorage.getItem", TokenKey);
+            Token = token;
         }
 
         public void ClearToken()
         {
             Token = "";
-            _ = _jsRuntime.InvokeVoidAsync("localStorage.removeItem", TokenKey);
+            _ = jsRuntime.InvokeVoidAsync("localStorage.removeItem", TokenKey);
         }
+
+        public bool IsAdmin => Role == "Admin";
+        public bool IsAdder => Role == "Adder";
     }
 }
